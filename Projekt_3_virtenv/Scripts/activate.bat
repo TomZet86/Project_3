51c39b9e78196d0ee7c5ccd6529d892387897909
TomZet86 <127389323+TomZet86@users.noreply.github.com>
2023-06-15 17:14:42 +0200

    new commit
    new commit

--- Projekt_3_virtenv/Scripts/activate.bat
+++ Projekt_3_virtenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\tzech\GitHub\Python\Projects\Projekt_3_virtenv
+set VIRTUAL_ENV=C:\Users\tzech\OneDrive\Dokumenty\GitHub\Project_3\Projekt_3_virtenv
 
 if not defined PROMPT set PROMPT=$P$G
 
